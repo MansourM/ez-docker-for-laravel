d9177acaf7429f5dc94164d128b9dc9fd08daf8f
MansourM <sm.mirbehbahani@gmail.com>
2023-11-26 05:03:26 +0330

    :bug: bugfix

--- entrypoint-laravel.sh
+++ entrypoint-laravel.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#if APP_ENV is not production and APP_DEBUG is true seed database
+if [ "$APP_ENV" != "production" ] && [ "$APP_DEBUG" = "true" ]; then
+    php artisan migrate:fresh --seed
+fi
+
 php artisan serve --port=8000 --host=0.0.0.0 --env=.env
