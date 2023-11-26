f69dd6659bb9c18ddb05b383c7ce2f88205eb2c9
MansourM <sm.mirbehbahani@gmail.com>
2023-11-26 05:27:29 +0330

    :arrow_up_small: removing profile so we can merge to main the changes we made so far.

--- entrypoint-laravel.sh
+++ entrypoint-laravel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#if APP_ENV is not production and APP_DEBUG is true seed database
-if [ "$APP_ENV" != "production" ] && [ "$APP_DEBUG" = "true" ]; then
+if [[ "$APP_ENV" == "dev" || "$APP_ENV" == "staging" ]]; then
     php artisan migrate:fresh --seed
+else
+    php artisan migrate
 fi
 
 php artisan serve --port=8000 --host=0.0.0.0 --env=.env
