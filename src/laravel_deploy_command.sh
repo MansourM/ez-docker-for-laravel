f18a82fea494e95cd4f0809e323eacb158cebec3
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2024-01-25 18:37:57 +0330

    :new: added profiles to all stages

--- src/laravel_deploy_command.sh
+++ src/laravel_deploy_command.sh
@@ -1,13 +1,20 @@
 #inspect_args
 
+#TODO read APP_ENV from cli args and ignore APP_ENV in .env?
+# Check if APP_ENV is set to dev, test, staging, or production
+if [[ "$APP_ENV" != "dev" && "$APP_ENV" != "test" && "$APP_ENV" != "staging" && "$APP_ENV" != "production" ]]; then
+    echo "Error: Invalid value for APP_ENV. It must be either dev, test, staging, or production."
+    exit 1
+fi
+
 #careful with laravel_folder_name, it must be the same as laravel dockerfile and docker compose file
-laravel_folder_name="laravel"
+laravel_folder_name="laravel-$APP_ENV"
 
 # Check if the folder exists
 if [ -d "$laravel_folder_name" ]; then
     echo "Updating existing $laravel_folder_name folder..."
     cd "$laravel_folder_name" || exit 1
-    git pull origin "$GIT_BRANCH"
+    git pull origin "$APP_ENV"
 
     # Check if the git operation was successful
     if [ $? -ne 0 ]; then
@@ -27,7 +34,7 @@ if [ -d "$laravel_folder_name" ]; then
     cd - || exit 1
 else
     echo "Cloning new $laravel_folder_name folder..."
-    git clone --depth 1 -b "$GIT_BRANCH" "$GIT_URL" "$laravel_folder_name"
+    git clone --depth 1 -b "$APP_ENV" "$GIT_URL" "$laravel_folder_name"
 
     # Check if cloning was successful
     if [ $? -ne 0 ]; then
@@ -37,14 +44,14 @@ else
 fi
 
 # Copy necessary files to the Laravel folder
+#TODO separate .env file for each environment?
 copy_file ".env" "$laravel_folder_name" ".env"
-copy_file "entrypoint-builder.sh" "$laravel_folder_name" "entrypoint-builder.sh"
-copy_file "entrypoint-laravel.sh" "$laravel_folder_name" "entrypoint-laravel.sh"
-
+copy_file "entrypoint-builder-$APP_ENV.sh" "$laravel_folder_name" "entrypoint-builder.sh"
+copy_file "entrypoint-laravel-$APP_ENV.sh" "$laravel_folder_name" "entrypoint-laravel.sh"
 
 # Run Docker Compose
 echo "Running Docker Compose for builder..."
-docker compose -f compose-builder.yml up --build
+docker compose -f compose-builder.yml --profile "$APP_ENV" up --build
 
 echo "Running Docker Compose for Laravel in detached mode..."
-docker compose -f compose-laravel.yml up --build -d
+docker compose -f compose-laravel.yml --profile "$APP_ENV" up --build -d
