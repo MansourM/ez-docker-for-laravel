1b8fa95892a9b0c3788ce192dd79e8ce06073ce3
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2024-02-29 15:11:54 +0330

    :arrow_up_small: bugfix amd better .env handling

--- src/before.sh
+++ src/before.sh
@@ -6,12 +6,11 @@
 ## You can safely delete this file if you do not need it.
 inspect_args
 
+#TODO improve here i have duplication in app_env arg and APP_ENV in .env files
 read_env "env/.env"
 read_env "env/shared.env"
 
-#TODO improve here i have duplication in app_env arg and APP_ENV in .env files
-if [[ -z "${args[APP_ENV]}" ]]; then
-        echo "Error: 'APP_ENV' is not set or empty. Please set a valid argument." >&2
-        exit 1
+if [[ -n "${args[APP_ENV]}" ]]; then
+  read_env "env/${args[APP_ENV]}.env"
 fi
-read_env "env/${args[APP_ENV]}.env"
+
