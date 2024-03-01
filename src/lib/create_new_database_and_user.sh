8fa7f761d0e3b26f5b3b99111b576d3b4b476389
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2024-03-01 20:05:35 +0330

    :bug: bugfix

--- src/lib/create_new_database_and_user.sh
+++ src/lib/create_new_database_and_user.sh
@@ -38,20 +38,12 @@ create_new_database_and_user() {
     log "Database: $NEW_DB_NAME Already Exists";
   fi
 
-  PMA_CONTAINER_NAME="pma"
-  if [[ "$APP_ENV" = "test" ]]; then
-    PMA_CONTAINER_NAME="pma-test"
-  fi
-
+  #TODO how to give access to specific container instead of %?
   # Check if user exists (improve error handling)
   if ! docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "SELECT user FROM mysql.user WHERE user='$NEW_USER_NAME';" --skip-column-names -B | grep -q "$NEW_USER_NAME"; then
     # Create user and grant privileges
-    if ! sudo docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "CREATE USER '$NEW_USER_NAME'@'localhost' IDENTIFIED BY '$NEW_USER_PASSWORD';" || \
-       ! sudo docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "CREATE USER '$NEW_USER_NAME'@'$PMA_CONTAINER_NAME' IDENTIFIED BY '$NEW_USER_PASSWORD';" || \
-       ! sudo docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "CREATE USER '$NEW_USER_NAME'@'$LARAVEL_CONTAINER_NAME' IDENTIFIED BY '$NEW_USER_PASSWORD';" || \
-       ! sudo docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`$NEW_DB_NAME\`.* TO '$NEW_USER_NAME'@'localhost';" || \
-       ! sudo docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`$NEW_DB_NAME\`.* TO '$NEW_USER_NAME'@'$PMA_CONTAINER_NAME';" || \
-       ! sudo docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`$NEW_DB_NAME\`.* TO '$NEW_USER_NAME'@'$LARAVEL_CONTAINER_NAME';" || \
+    if ! sudo docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "CREATE USER '$NEW_USER_NAME'@'%' IDENTIFIED BY '$NEW_USER_PASSWORD';" || \
+       ! sudo docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`$NEW_DB_NAME\`.* TO '$NEW_USER_NAME'@'%';" || \
        ! sudo docker exec -i $DB_HOST mysql -u$MYSQL_USER -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"; then
       log_error "Failed to create and grant privileges for user '$NEW_USER_NAME'"
       return 1
