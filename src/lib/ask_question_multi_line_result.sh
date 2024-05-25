b7a00a0c1d59fc62c3818998f58f7878d429dc4d
MansourM <sm.mirbehbahani@gmail.com>
2024-05-25 10:58:58 +0330

    :arrow_up_small: WIP laravel new

--- src/lib/ask_question_multi_line_result.sh
+++ src/lib/ask_question_multi_line_result.sh
@@ -3,11 +3,9 @@ ask_question_multi_line_result() {
     local delimiter="ez;"
     local result
 
-    # Colors
     local GREEN='\033[0;32m'
     local NC='\033[0m' # No Color
 
-    # Prompt the user to enter the content
     echo -e "${GREEN}$question (type '$delimiter' on a new line to finish)${NC}"
     result=""
     while IFS= read -r line; do
