195be3143d464d0df2762495fdef9d0b0f1fad1c
MansourM <sm.mirbehbahani@gmail.com>
2024-05-25 08:47:10 +0330

    :new: WIP ask_question

--- src/laravel_new_command.sh
+++ src/laravel_new_command.sh
@@ -7,6 +7,12 @@ DEFAULT_APP_PORT="8080"
 APP_NAME=$(ask_question "Enter the application name" $DEFAULT_APP_NAME)
 APP_PORT=$(ask_question "Enter the application port" $DEFAULT_APP_PORT)
 
-echo $APP_NAME
+echo "$APP_NAME"
+echo "$APP_PORT"
 
-echo $APP_PORT
+DATA_DIR="data/$APP_NAME"
+
+if [[ -d "$DATA_DIR" ]]; then
+    log_error "Directory $DATA_DIR already exists. Exiting."
+    exit 1
+fi
