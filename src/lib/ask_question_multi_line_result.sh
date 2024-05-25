be9d406654a6d45b3f552ebb24fef54d7d1548b4
MansourM <sm.mirbehbahani@gmail.com>
2024-05-25 15:01:00 +0330

    :arrow_up_small: WIP laravel new

--- src/lib/ask_question_multi_line_result.sh
+++ src/lib/ask_question_multi_line_result.sh
@@ -4,13 +4,11 @@ ask_question_multi_line_result() {
     local result
 
     local GREEN='\033[0;32m'
+    local YELLOW='\033[0;33m'
     local NC='\033[0m' # No Color
 
-    # Display the question to the user
-    echo -e "${GREEN}$question (type '$delimiter' on a new line to finish)${NC}"
-
-    # Prompt the user to paste the content
-    echo "Paste your content here. Press Enter after the last line, then type '$delimiter' and press Enter again to finish."
+    # Display the question to the user before starting the loop
+    echo -e "${GREEN}$question ${YELLOW}[type '$delimiter' on a new line to finish]${NC}"
 
     result=""
     while IFS= read -r line; do
