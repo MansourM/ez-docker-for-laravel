0f022fd2d94eca35851429682ac06bb9b966221b
MansourM <sm.mirbehbahani@gmail.com>
2024-05-26 04:40:08 +0330

    :arrow_up_small: WIP laravel new

--- src/laravel_new_command.sh
+++ src/laravel_new_command.sh
@@ -6,7 +6,17 @@ echo -e "$PROMPT"
 ENV_CONTENT=$(read_multi_line_input "$DELIMITER")
 
 APP_NAME=$(echo "$ENV_CONTENT" | grep -oP '^APP_NAME=\K.*')
-APP_NAME=$(ask_question "is this you app name? app names on this machine must be unique" "$APP_NAME")
+APP_NAME=$(ask_question "Enter the application name" "$APP_NAME")
+
+DATA_DIR="data/$APP_NAME"
+
+if [[ -d "$DATA_DIR" ]]; then
+    log_error "Directory $DATA_DIR already exists app_name must be unique. Exiting."
+    exit 1
+fi
+
+
+
 GIT_URL=$(ask_question "Enter the application git url" "https://github.com/MansourM/ez-docker-for-laravel-example.git")
 SETUP_TEST_ENV=$(ask_question "Do you want to set up the test environment?" "yes")
 
@@ -15,25 +25,11 @@ echo "$GIT_URL"
 echo "$SETUP_TEST_ENV"
 
 if [[ "$SETUP_TEST_ENV" == "yes" ]]; then
-
-    TEST_BRANCH_NAME=$(ask_question "Enter the test branch name" "test")
-
-    TEST_DB_NAME=$(ask_question "Enter the test database name" "${APP_NAME}_test")
-    TEST_DB_USERNAME=$(ask_question "Enter the test database username" "${APP_NAME}_test_user")
-    GENERATED_PASSWORD=$(generate_password 16)
-    TEST_DB_PASSWORD=$(ask_question "Enter the $TEST_DB_USERNAME 's password'" "$GENERATED_PASSWORD")
-
-    echo "$TEST_BRANCH_NAME"
-    echo "$TEST_DB_NAME"
-    echo "$TEST_DB_USERNAME"
-    echo "$TEST_DB_PASSWORD"
+  setup_environment "$APP_NAME" "test"
 fi
 
+SETUP_STAGING_ENV=$(ask_question "Do you want to set up the test environment?" "yes")
 
-
-DATA_DIR="data/$APP_NAME"
-
-if [[ -d "$DATA_DIR" ]]; then
-    log_error "Directory $DATA_DIR already exists. Exiting."
-    exit 1
+if [[ "$SETUP_STAGING_ENV" == "yes" ]]; then
+  setup_environment "$APP_NAME" "staging"
 fi
