a1034ae261a708023f4944da9fc247392ddc98a0
MansourM <sm.mirbehbahani@gmail.com>
2024-05-26 06:23:06 +0330

    :arrow_up_small: WIP laravel new

--- src/laravel_new_command.sh
+++ src/laravel_new_command.sh
@@ -28,8 +28,14 @@ if [[ "$SETUP_TEST_ENV" == "yes" ]]; then
   setup_environment "$APP_NAME" "test"
 fi
 
-SETUP_STAGING_ENV=$(ask_question "Do you want to set up the test environment?" "yes")
+SETUP_STAGING_ENV=$(ask_question "Do you want to set up the staging environment?" "yes")
 
 if [[ "$SETUP_STAGING_ENV" == "yes" ]]; then
   setup_environment "$APP_NAME" "staging"
+fi
+
+SETUP_STAGING_ENV=$(ask_question "Do you want to set up the production environment?" "yes")
+
+if [[ "$SETUP_STAGING_ENV" == "yes" ]]; then
+  setup_environment "$APP_NAME" "production"
 fi
