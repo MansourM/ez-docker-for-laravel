cebb12d08ebb74ade4ad99cdc98ee9b726714809
MansourM <sm.mirbehbahani@gmail.com>
2024-05-29 14:09:43 +0330

    :arrow_up_small: WIP laravel deploy

--- src/lib/check_containers.sh
+++ src/lib/check_containers.sh
@@ -1,9 +1,10 @@
 check_containers() {
+  log_info "Checking shared containers health before building the laravel container."
     local containers=("$@")
 
     for container in "${containers[@]}"; do
         if is_container_running "$container"; then
-            log_success "Container '$container' is running."
+            log_success "Container '$container' is running, you can run shared container by './ez shared deploy'"
             if is_container_healthy "$container"; then
                 log_success "Container '$container' is healthy."
             else
