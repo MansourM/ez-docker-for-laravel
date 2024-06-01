f9e79ae23e4d62bbfd5abf4c51a8387f8f1f9b61
MansourM <sm.mirbehbahani@gmail.com>
2024-06-01 08:14:28 +0330

    :arrow_up_small: improving check_containers fn

--- src/lib/check_containers2.sh
+++ src/lib/check_containers2.sh
@@ -1,25 +1,25 @@
 check_containers2() {
-    log_info "Checking shared containers health before building the laravel container."
+    log_info "Checking shared containers health before building the Laravel container."
     local containers=("$@")
     local retries=3
     local delay=10
 
     for container in "${containers[@]}"; do
-        for attempt in $(seq 1 $retries); do
+        for attempt in $(seq 1 $((retries + 1))); do
             if is_container_running "$container"; then
-                log_success "Container '$container' is running.'"
+                log_success "Container '$container' is running."
                 if is_container_healthy "$container"; then
                     log_success "Container '$container' is healthy."
                     break
                 else
-                    log_warning "Container '$container' is not healthy. Attempt $attempt/$retries."
+                    log_warning "Container '$container' is not healthy. Attempt $((attempt-1))/$retries."
                 fi
             else
-                log_warning "Container '$container' is not running. Attempt $attempt/$retries. You can run shared container by './ez shared deploy'"
+                log_warning "Container '$container' is not running. Attempt $((attempt-1))/$retries. You can run shared container by './ez shared deploy'"
             fi
 
-            if [ $attempt -eq $retries ]; then
-                log_error "Container '$container' is not running or not healthy after $retries attempts."
+            if [ $attempt -gt $retries ]; then
+                log_error "Container '$container' is not healthy or not running after $retries attempts."
                 exit 1
             fi
 
