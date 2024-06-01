ff3ce69e491ae800345b674d5d7fd84709ca1aaf
MansourM <sm.mirbehbahani@gmail.com>
2024-06-01 09:19:06 +0330

    :arrow_up_small: improving check_containers done

--- src/lib/check_containers.sh
+++ src/lib/check_containers.sh
@@ -1,19 +1,34 @@
 check_containers() {
-  log_info "Checking shared containers health before building the laravel container."
+    log_info "Checking shared containers health before building the Laravel container."
     local containers=("$@")
+    local retries=3
+    local delay=10
 
     for container in "${containers[@]}"; do
-        if is_container_running "$container"; then
-            log_success "Container '$container' is running.'"
-            if is_container_healthy "$container"; then
-                log_success "Container '$container' is healthy."
+        for attempt in $(seq 1 $((retries + 1))); do
+            if is_container_running "$container"; then
+                log_success "Container '$container' is running."
+                if is_container_healthy "$container"; then
+                    log_success "Container '$container' is healthy."
+                    break
+                else
+                    log_warning "Container '$container' is not healthy. Attempt $((attempt-1))/$retries."
+                fi
             else
-                log_error "Container '$container' is not healthy."
+                log_warning "Container '$container' is not running. Attempt $((attempt-1))/$retries. You can run shared container by './ez shared deploy'"
+            fi
+
+            if [ $attempt -gt $retries ]; then
+                log_error "Container '$container' is not healthy or not running after $retries attempts."
                 exit 1
             fi
-        else
-            log_error "Container '$container' is not running, you can run shared container by './ez shared deploy"
-            exit 1
-        fi
+
+            log_info "Retrying in $delay seconds: "
+            for ((i=0; i<$delay; i++)); do
+                echo -n "."
+                sleep 1
+            done
+            echo ""
+        done
     done
 }
