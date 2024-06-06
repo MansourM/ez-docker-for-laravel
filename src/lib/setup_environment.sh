40d4943f5aeb59d3f564649b84464a0fbd2943cf
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2024-06-06 11:59:35 +0330

    :bug: fixing heredoc bug

--- src/lib/setup_environment.sh
+++ src/lib/setup_environment.sh
@@ -1,34 +1,34 @@
 setup_environment() {
-    local app_name=$1
-    local environment=$2
+  local app_name=$1
+  local environment=$2
 
-    local branch_name db_database db_username generated_password db_password app_debug
+  local branch_name db_database db_username generated_password db_password app_debug
 
-    if [ "$environment" == "production" ]; then
-      branch_name="main"
-    else
-      branch_name="$environment"
-    fi
+  if [ "$environment" == "production" ]; then
+    branch_name="main"
+  else
+    branch_name="$environment"
+  fi
 
-    branch_name=$(ask_question "Enter the ${environment} branch name" "$branch_name")
+  branch_name=$(ask_question "Enter the ${environment} branch name" "$branch_name")
 
-    db_database=$(ask_question "Enter the ${environment} database name" "${app_name}_${environment}")
-    db_username=$(ask_question "Enter the ${environment} database username" "${app_name}_${environment}_user")
-    generated_password=$(generate_password 20)
-    db_password=$(ask_question "Enter the $db_username's password" "$generated_password")
+  db_database=$(ask_question "Enter the ${environment} database name" "${app_name}_${environment}")
+  db_username=$(ask_question "Enter the ${environment} database username" "${app_name}_${environment}_user")
+  generated_password=$(generate_password 20)
+  db_password=$(ask_question "Enter the $db_username's password" "$generated_password")
 
-    if [ "$environment" == "test" ]; then
-      app_debug=true
-      # Determine the test app port based on the number of folders in the apps directory
-      local num_apps=$(ls -l apps | grep -c '^d')
-      local default_port=$((7999 + num_apps))
-      app_port=$(ask_question "Enter the test app port" "$default_port")
-    else
-      app_debug=false
-    fi
+  if [ "$environment" == "test" ]; then
+    app_debug=true
+    # Determine the test app port based on the number of folders in the apps directory
+    local num_apps=$(ls -l apps | grep -c '^d')
+    local default_port=$((7999 + num_apps))
+    app_port=$(ask_question "Enter the test app port" "$default_port")
+  else
+    app_debug=false
+  fi
 
-#FIXME do something for APP_URL
-    cat <<EOL > "apps/$app_name/env/$environment.env"
+  #FIXME do something for APP_URL
+  cat <<EOL > "apps/$app_name/env/$environment.env"
 GIT_BRANCH=$branch_name
 APP_ENV=$environment
 APP_DEBUG=$app_debug
