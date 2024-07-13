ce79365b528615f518ad108e7634ff40f6ea0505
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2024-07-13 16:17:32 +0330

    :new: WIP exploring

--- src/before.sh
+++ src/before.sh
@@ -4,9 +4,11 @@
 ## before running any command (but after processing its arguments).
 ##
 ## You can safely delete this file if you do not need it.
-#inspect_args
+inspect_args
 
-if [[ $EUID -ne 0 ]]; then
-   log_error "This script must be run as root"
-   exit 1
-fi
+eval "$(send_completions)"
+
+#if [[ $EUID -ne 0 ]]; then
+#   log_error "This script must be run as root"
+#   exit 1
+#fi
---
+++ src/test_install_command.sh
@@ -0,0 +1,6 @@
+echo "# this file is located in 'src/test_install_command.sh'"
+echo "# code for 'ez test install' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
+
+echo "test install"
