d0b58ea2f47b4068b212470b9c5a3f75947b960d
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2024-08-10 15:40:30 +0330

    :bug: added missing .env file gen/load to other laravel commands

--- src/laravel_down_command.sh
+++ src/laravel_down_command.sh
@@ -1,2 +1,4 @@
-#TODO add better logs to laravel commands?
-docker compose -f "apps/${args[APP_NAME]}/compose-laravel.yml" --profile "${args[APP_ENV]}" down
+app_dir="apps/${args[APP_NAME]}"
+merged_env_path=$(load_laravel_envs "$app_dir" "${args[APP_ENV]}")
+
+docker compose -f "app_dir/compose-laravel.yml" --profile "${args[APP_ENV]}" --env-file "$merged_env_path" down
--- src/laravel_start_command.sh
+++ src/laravel_start_command.sh
@@ -1,1 +1,4 @@
-docker compose -f "apps/${args[APP_NAME]}/compose-laravel.yml" --profile "${args[APP_ENV]}" start
+app_dir="apps/${args[APP_NAME]}"
+merged_env_path=$(load_laravel_envs "$app_dir" "${args[APP_ENV]}")
+
+docker compose -f "apps/${args[APP_NAME]}/compose-laravel.yml" --profile "${args[APP_ENV]}" --env-file "$merged_env_path" start
