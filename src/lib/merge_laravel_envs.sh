8b1ff3c4ea1b2ee4e1bc82301d2ee1aa2d116d3f
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2024-08-10 16:00:31 +0330

    :memo: bugfix

--- src/lib/merge_laravel_envs.sh
+++ src/lib/merge_laravel_envs.sh
@@ -2,11 +2,13 @@ merge_laravel_envs() {
   local app_dir=$1
   local app_env=$2
 
+  laravel_env_path="$app_dir/env/laravel.env"
   docker_env_path="docker/docker.env"
   app_env_path="$app_dir/env/app.env"
+  override_env_path="$app_dir/env/$app_env.env"
   merged_env_path="$app_dir/env/generated/$app_env.env"
 
-  load_env "$docker_env_path"
-  load_env "$app_env_path"
-  load_env "$merged_env_path"
+  merge_envs "$merged_env_path" "$laravel_env_path" "$docker_env_path" "$app_env_path" "$override_env_path"
+
+  echo "$merged_env_path"
 }
