c75e30d9c85f319fc8a39b35099b6b37263aa1d9
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2024-08-11 14:28:09 +0330

    :arrow_up_small: copy .env file and redo chown for dev env on start / restart (deploy already had it)

--- src/laravel_restart_command.sh
+++ src/laravel_restart_command.sh
@@ -2,4 +2,9 @@ app_dir="apps/${args[APP_NAME]}"
 merged_env_path=$(merge_laravel_envs "$app_dir" "${args[APP_ENV]}")
 load_laravel_envs "$app_dir" "${args[APP_ENV]}"
 
+if [[ "${args[APP_ENV]}" == "dev" ]]; then
+  cp "$merged_env_path" "$app_dir/src-dev/.env"
+  chown -R "$OWNER_USER_NAME:$OWNER_GROUP_NAME" "$app_dir/src-dev"
+fi
+
 docker compose -f "$app_dir/compose-laravel.yml" --profile "${args[APP_ENV]}" --env-file "$merged_env_path" restart
