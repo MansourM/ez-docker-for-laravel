9fc1c628c5b29ce9925243ec0ed6d5e236d22331
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2024-12-28 12:43:56 +0330

    :arrow_up_small: added filament and filament shield commands to entry point (commented)

--- template/entrypoint-dev.sh
+++ template/entrypoint-dev.sh
@@ -12,5 +12,9 @@ npm run build;
 php artisan key:generate
 php artisan migrate:fresh --seed
 
+#add these or other similar ones here if needed
+#php artisan optimize
+#php artisan filament:optimize
+#php artisan shield:generate --all
 
 supervisord -c /etc/supervisor/supervisord.conf
