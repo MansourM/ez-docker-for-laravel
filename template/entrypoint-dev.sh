bcae3472f247e5be983038f0d1e4daefb1bdc837
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2025-01-27 21:12:26 +0330

    :bug: added fixme

--- template/entrypoint-dev.sh
+++ template/entrypoint-dev.sh
@@ -12,6 +12,7 @@ npm run build;
 php artisan key:generate
 php artisan migrate:fresh --seed
 
+#FIXME this is buggy since there is no optimize:clear specially on auto load and composer stuff
 #add these or other similar ones here if needed
 #php artisan optimize
 #php artisan filament:optimize
