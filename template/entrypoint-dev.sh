f19c6edf28b24f90bea70fde56fd93d7968a2120
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2025-02-06 18:05:50 +0330

    :arrow_up_small: WIP supervisord

--- template/entrypoint-dev.sh
+++ template/entrypoint-dev.sh
@@ -11,8 +11,8 @@ npm run build;
 
 php artisan key:generate
 php artisan migrate:fresh --seed
+php artisan optimize:clear
 
-#FIXME this is buggy since there is no optimize:clear specially on auto load and composer stuff
 #add these or other similar ones here if needed
 #php artisan optimize
 #php artisan filament:optimize
