77b5832ba7e9172174539f4279286305e90b139d
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2025-02-25 15:30:09 +0330

    :arrow_up_small: improved docker files and entrypoint

--- template/entrypoint.sh
+++ template/entrypoint.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 app_env=${APP_ENV}
 
-if [[ "$app_env" == "test" ]]; then
-    echo "fresh database + seed ..."
-    php artisan migrate:fresh --seed
-else # staging or production
-  echo "migrating database..."
-    php artisan migrate
+if [ -z "$app_env" ]; then
+    echo "Error: APP_ENV is not set."
+    exit 1
 fi
 
+# Generate Laravel key if it doesn't exist
+if [ -z "$(grep '^APP_KEY=' .env)" ]; then
+    echo "Generating Laravel application key..."
+    php artisan key:generate
+fi
+
+case "$app_env" in
+    "local" | "dev" | "test")
+        echo "fresh database + seed ..."
+        php artisan migrate:fresh --seed
+        ;;
+    "staging")
+        echo "migrating database in staging --force..."
+        php artisan migrate --force
+        ;;
+    "production")
+        if composer show spatie/laravel-backup > /dev/null 2>&1; then
+            echo "Backing up database..."
+            php artisan backup:run --only-db
+
+            echo "Migrating database in production..."
+            php artisan migrate --force
+        else
+            echo "Error: spatie/laravel-backup is not installed. Skipping migrations."
+            echo "Please handle migrations manually after ensuring a backup is available."
+            exit 1
+        fi
+        ;;
+    *)
+        echo "Unknown environment: $app_env"
+        exit 1
+        ;;
+esac
 
 supervisord -c /etc/supervisor/supervisord.conf
